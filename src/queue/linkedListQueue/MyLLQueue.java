afb529531dda36f8a6f8201503e2b6375130d29c
markbotros0 <MarkBotros0@gmail.com>
2024-03-19 22:26:40 +0200

    improve some bugs

--- src/queue/linkedListQueue/MyLLQueue.java
+++ src/queue/linkedListQueue/MyLLQueue.java
@@ -9,6 +9,7 @@ public class MyLLQueue<T> {
     private SingleNode<T> first;
     private SingleNode<T> last;
     private int length;
+
     public int getLength() {
         return length;
     }
@@ -16,7 +17,7 @@ public class MyLLQueue<T> {
     // O(1)
     public void enqueue(T input) {
         SingleNode<T> newNode = new SingleNode<>(input);
-        if (this.first == null){
+        if (this.first == null) {
             this.first = newNode;
         } else {
             this.last.setNextNode(newNode);
@@ -26,9 +27,17 @@ public class MyLLQueue<T> {
     }
 
     // O(1)
-    public void dequeue() {
+    public T dequeue() {
+        if (this.first == null) {
+            return null;
+        }
+        if (this.first == this.last) {
+            this.last = null;
+        }
+        SingleNode<T> holdingPointer = this.first;
         this.first = this.first.getNextNode();
         this.length--;
+        return holdingPointer.getValue();
     }
 
     // O(1)
@@ -41,7 +50,7 @@ public class MyLLQueue<T> {
         return this.first == null;
     }
 
-    public void printQueue(){
+    public void printQueue() {
         ArrayList<String> list = new ArrayList<>();
         SingleNode<T> currNode = this.first;
         while (currNode != null) {
@@ -50,7 +59,6 @@ public class MyLLQueue<T> {
         }
         System.out.println(list);
     }
-
 
     public static void testQueue() {
         MyLLQueue<String> queue = new MyLLQueue<>();
@@ -66,6 +74,7 @@ public class MyLLQueue<T> {
         queue.dequeue();
         queue.dequeue();
         queue.dequeue();
+        System.out.println(queue.dequeue());
         queue.dequeue();
         System.out.println(queue.peek());
 
