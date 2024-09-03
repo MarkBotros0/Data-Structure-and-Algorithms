9ce99eb76d53d345887adec552ffe555b8fc2d1a
markbotros0 <markbotro0@gmail.com>
2024-09-03 13:25:55 +0300

    Add: more leetcode problems

--- src/leetcode/_3Sum.java
+++ src/leetcode/_3Sum.java
@@ -1,28 +1,36 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 //15
 public class _3Sum implements TestSolution {
 
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> arrayOfTriplets = new ArrayList<>();
-        int firstIndex = 0;
-        int middleIndex = 1;
-        int lastIndex = nums.length - 1;
-        int sumOfTwoIntegers = nums[firstIndex] + nums[lastIndex];
-        while (lastIndex - firstIndex >= 2) {
-            if (sumOfTwoIntegers + nums[middleIndex] == 0) {
-                List<Integer> list = new ArrayList<>(Arrays.asList(nums[firstIndex], nums[middleIndex], nums[lastIndex]));
-                arrayOfTriplets.add(list);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
             }
-            middleIndex++;
-            if (lastIndex == middleIndex) {
-                lastIndex--;
-                middleIndex = firstIndex + 1;
-                sumOfTwoIntegers = nums[firstIndex] + nums[lastIndex];
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    arrayOfTriplets.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
             }
         }
         return arrayOfTriplets;
@@ -31,6 +39,7 @@ public class _3Sum implements TestSolution {
     @Override
     public void test() {
         int[] numbers = {-1, 0, 1, 2, -1, -4};
-        System.out.println(threeSum(numbers));
+        int[] numbers2 = {0, 0, 0, 0};
+        System.out.println(threeSum(numbers2));
     }
 }
