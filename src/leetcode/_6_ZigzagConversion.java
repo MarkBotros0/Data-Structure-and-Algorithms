ff99a33f8b4d71161ab61f2bf1b1f61e0d06189a
markbotros0 <markbotro0@gmail.com>
2024-09-11 09:40:41 +0300

    Add: more leetcode problems

--- src/leetcode/_6_ZigzagConversion.java
+++ src/leetcode/_6_ZigzagConversion.java
@@ -5,41 +5,34 @@ import java.util.List;
 
 public class _6_ZigzagConversion implements TestSolution {
 
-/*
-(1, 1)
-(3, 4)
-(4, 6)
-(5, 8)
-(6, 10)
-(n, 2*(n-1))
- */
-
-
     public String convert(String s, int numRows) {
-        StringBuilder finalWord = new StringBuilder();
-        List<StringBuilder> list = new ArrayList<>();
+        if (numRows == 1) {
+            return s;
+        }
+        List<StringBuilder> sbArr = new ArrayList<>();
+        int counter = 0;
+        int variance = 1;
         for (int i = 0; i < numRows; i++) {
-            int index = i;
-            StringBuilder sb = new StringBuilder();
-            list.add(sb);
-            while (index < s.length()) {
-                if (){
-                sb.append(s.charAt(index));
-                index = index + (2 * (numRows - 1));
-                }
+            sbArr.add(new StringBuilder());
+        }
+        for (int i = 0; i < s.length(); i++) {
+            sbArr.get(counter).append(s.charAt(i));
+            counter += variance;
+            if (counter == numRows - 1) {
+                variance = -1;
+            } else if (variance == -1 && counter == 0) {
+                variance = 1;
             }
-            index = i;
         }
-
-        for (StringBuilder sb : list) {
-            finalWord.append(sb);
+        StringBuilder finalString = new StringBuilder();
+        for (StringBuilder sb : sbArr) {
+            finalString.append(sb.toString());
         }
-
-        return finalWord.toString();
+        return finalString.toString();
     }
 
     @Override
     public void test() {
-        System.out.println(convert("PAYPALISHIRING", 3));
+        System.out.println(convert("PAYPALISHIRING", 4));
     }
 }
